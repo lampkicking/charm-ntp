289124d39400f785e9a51dca48b636dbd0ae5994
Brad Marshall <brad.marshall@canonical.com>
2015-03-27 13:59:37 +1000

    [bradm] Added default empty string to nagios_servicegroup, unit tests for peers

--- tests/10-deploy-test.py
+++ tests/10-deploy-test.py
@@ -5,31 +5,37 @@
 import amulet
 
 # The number of seconds to wait for Juju to set up the environment.
-seconds = 900
+seconds = 9600
 ntp_server_0 = 'ntp.your.org'
 ntp_server_1 = 'us.pool.ntp.org'
 ntp_server_2 = 'tock.mtnlion.com'
 
 # The ntp configuration to test.
 ntp_configuration = {
-    'source': ntp_server_0 + ' ' + ntp_server_1 + ' ' + ntp_server_2
+    'source': ntp_server_0 + ' ' + ntp_server_1 + ' ' + ntp_server_2,
+    'auto_peers': True
 }
 
-d = amulet.Deployment()
+d = amulet.Deployment(series='trusty', juju_deployer='juju-deployer -d -v')
+print("Adding instances")
 # Add the ntp charm to the deployment.
 d.add('ntp')
 # Add the ntpmaster charm to the deployment.
 d.add('ntpmaster')
 # Add the ubuntu charm to the deployment.
-d.add('ubuntu')
+d.add('ubuntu', units=2)
+#d.add('ubuntu')
+print("Configuring charms")
 # Configure the ntp charm.
 d.configure('ntp', ntp_configuration)
 
+print("Relating charms")
 # Relate the ntp and ntpmaster charms.
 d.relate('ntp:master', 'ntpmaster:master')
 # Relate the ntp and the ubuntu charm.
 d.relate('ntp:juju-info', 'ubuntu:juju-info')
 
+print("Deploying environment")
 # Deploy the environment and wait for it to setup.
 try:
     d.setup(timeout=seconds)
@@ -40,6 +46,7 @@ except amulet.helpers.TimeoutError:
     amulet.raise_status(amulet.SKIP, msg=message)
 except:
     raise
+print("Done deploying environment")
 
 # Unable to get the sentry unit for ntp because it is a subordinate.
 # ntp_unit = d.sentry.unit['ntp/0']
@@ -48,7 +55,8 @@ except:
 ntpmaster_unit = d.sentry.unit['ntpmaster/0']
 
 # Get the sentry unit for ubuntu.
-ubuntu_unit = d.sentry.unit['ubuntu/0']
+ubuntu_unit_0 = d.sentry.unit['ubuntu/0']
+ubuntu_unit_1 = d.sentry.unit['ubuntu/1']
 
 # Get the public address for the system running the ntmpmaster charm.
 master_public_address = ntpmaster_unit.info['public-address']
@@ -59,11 +67,16 @@ master_relation = ntpmaster_unit.relation('master', 'ntp:master')
 master_private_address = master_relation['private-address']
 print('ntpmaster private address ' + master_private_address)
 
+ubuntu_0_public_address = ubuntu_unit_0.info['public-address']
+print('ubuntu/0 public address ' + ubuntu_0_public_address)
+ubuntu_1_public_address = ubuntu_unit_1.info['public-address']
+print('ubuntu/1 public address ' + ubuntu_1_public_address)
+
 # Create a command to check the ntp service.
 command = 'sudo service ntp status'
 print(command)
-# Run the command to see if apache2 is running.
-output, code = ubuntu_unit.run(command)
+# Run the command to see if ntp is running.
+output, code = ubuntu_unit_0.run(command)
 print(output)
 if code != 0 or output.find('NTP server is running') == -1:
     message = 'The NTP service is not running on the ubuntu unit.'
@@ -86,5 +99,9 @@ configuration_file.index(ntp_server_2)
 
 # Search for the ntpmaster IP address in the config file, added by relation.
 configuration_file.index(master_private_address)
+
+# Search for ubuntu/[01] in the config file, raise an exception if not found.
+configuration_file.index(ubuntu_0_public_address)
+configuration_file.index(ubuntu_1_public_address)
 
 print('The ntp deploy test completed successfully.')
