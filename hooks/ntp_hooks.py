253a3f3528256cb8067ee9d4d6ff1954650e8086
Stuart Bishop <stuart.bishop@canonical.com>
2016-09-28 18:33:51 +0700

    delint

--- hooks/ntp_hooks.py
+++ hooks/ntp_hooks.py
@@ -129,7 +129,8 @@ def update_nrpe_config():
         nrpe_setup.add_check(
             shortname="ntpmon",
             description='Check NTPmon {}'.format(current_unit),
-            check_cmd='check_ntpmon.py --checks ' + ' '.join(nagios_ntpmon_checks)
+            check_cmd=('check_ntpmon.py --checks ' +
+                       ' '.join(nagios_ntpmon_checks))
         )
 
     nrpe_setup.write()
